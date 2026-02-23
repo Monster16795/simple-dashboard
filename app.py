70e077d4837cab6002fb1a31739c07d34a7700cf
Monster16795 <robin16795@gmail.com>
2026-02-22 23:25:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def toggle_auto():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))  # Render provides $PORT
     app.run(host="0.0.0.0", port=port)
